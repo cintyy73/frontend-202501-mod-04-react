ed71c020bfdf395adde9d02a7c3d223eb046b115
cintyy73 <cintiapaezdm@gmail.com>
2025-10-01 22:12:36 -0300

    inicio de repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './components/Card'
+import { Button, ButtonPostulate } from './components/Button'
+
+function App() {
+
+  return (
+    <>
+      <Card/>
+      <Button />
+      <ButtonPostulate/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+const Card = () => {
+  return (
+    <div>
+      <h1>Alumnas de backend</h1>
+        <ul>
+            <li>Cintia</li> 
+            <li>Florencia</li>
+            <li>Gisele</li>A
+            <li>Jimena</li>
+            <li>Luciana</li>
+           
+        </ul>
+    </div>
+  )
+}
+
+export default Card
