3434546eea6f2e16900aa22955fbe33f766c6b5b
cintyy73 <cintiapaezdm@gmail.com>
2025-11-17 22:05:51 -0300

    Ejemplos de chakra en clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,29 @@
-import styled from "styled-components";
+import { Container, HStack, Text, VStack } from "@chakra-ui/react";
+import Card from "./components/Card";
+import Alert from "./components/Alert";
 
-const Button = styled.button`
-  background: ${(props) => (props.secondary ? "#6c757d" : "#007bff")};
-  color: white;
-  border: none;
-  padding: ${(props) => (props.size === "large" ? " 12px 24px" : "8px 16px")};
-  border-radius: 4px;
-  cursor: pointer;
-  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
-  pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")};
-  &:hover {
-    background: #0056b3;
-  }
-`;
-
-const BaseButton = styled.button`
-  padding: 12px 24px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
-
-const PrimaryButton = styled(BaseButton)`
-  background: #007bff;
-  color: white;
-
-  &:hover {
-    background: #0056b3;
-  }
-`;
-
-const SecondaryButton = styled(BaseButton)`
-  background: #1f0622ff;
-  color: white;
-`;
-
-const ButtonThemeDanger = styled.button`
-  background: ${(props) => props.theme.colors.danger};
-  padding: ${(props) => props.theme.spacing.xlarge};
-  @media (max-width: ${(props) => props.theme.breakpoints.mobile}) {
-    padding: ${(props) => props.theme.spacing.small};
-  }
-`;
- const device = {
-  mobile: `(max-width: 768px)`,
-  tablet: `(max-width: 1024px)`,
-  desktop: `(min-width: 1025px)`,
-};
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 24px;
-
-  @media ${device.tablet} {
-    grid-template-columns: repeat(2, 1fr);
-    gap: 16px;
-  }
-
-  @media ${device.mobile} {
-    grid-template-columns: 1fr;
-    gap: 12px;
-  }
-`;
-const InteractiveCard = styled.div`
-  background: white;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  transition: all 0.3s ease;
-  padding: 16px;
-
-  @media (max-width: 768px) {
-    background: red;
-  }
-
-  @media (min-width: 1200px) {
-    background: blue;
-  }
-  &:hover {
-    transform: translateY(-24px);
-    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-  }
-`;
- 
 function App() {
   return (
-    <div style={{ backgroundColor: "black", height: "110vh" }}>
-      <Grid>
-      <InteractiveCard>
-        <Button disabled>Disabled</Button>
-        <Button size="large">Click primary</Button>
-        <Button secondary>Click secondary</Button>
-        <hr />
-        <BaseButton>Base</BaseButton>
-        <PrimaryButton>primary extend base</PrimaryButton>
-        <SecondaryButton>Secondary extend base</SecondaryButton>
-        <hr />
-        <ButtonThemeDanger>theme Danger</ButtonThemeDanger>
-      </InteractiveCard>
-      </Grid>
-    </div>
+    <Container>
+      <Alert/>
+      <HStack>
+        <Text>1</Text>
+        <Text>2</Text>
+        <Text>3</Text>
+        <Text>4</Text>
+      </HStack>
+      <VStack>
+        <Text>1</Text>
+        <Text>2</Text>
+        <Text>3</Text>
+        <Text>4</Text>
+      </VStack>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </Container>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme.js";
+import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={theme}>
+  <ChakraProvider>
     <App />
-  </ThemeProvider>
+  </ChakraProvider>
 );
